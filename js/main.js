5643e9e0b2fda7f54adb6861074fb9973d79e4d0
Zahid Patoary <zahidpatoary@Zahids-MacBook-Air.local>
2022-03-01 12:27:51 +0900

    first

--- js/main.js
+++ js/main.js
@@ -8,6 +8,27 @@ const searchPhone = () => {
     .then(data => displaySearchResult(data.data))
     
 };
-const displaySearchResult = (data) => {
+const displaySearchResult = datas => {
+    console.log(datas)
+    const searchResult = document.getElementById('search-result');
+    searchResult.textContent = '';
+    
 
-}
+    datas.forEach(data => {
+        const div = document.createElement('div');
+    div.innerHTML = `
+    <div class="card h-100">
+            <img src="${data.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">${data.brand}</h5>
+              <p class="card-text">${data.phone_name}</p>
+            </div>
+          </div>
+    `;
+    searchResult.appendChild(div);
+    });
+    
+   
+};
+
+//display details
